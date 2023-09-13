3bd3121062642babf0d9d4c851e7421f79142a48
Jayanth <jay.codmik.18@gmail.com>
2023-09-14 00:34:30 +0530

    model updated

--- client/script.js
+++ client/script.js
@@ -86,7 +86,9 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://gptclone-20va.onrender.com/', {
+    // const response = await fetch('https://gptclone-20va.onrender.com/', {
+    const response = await fetch('http://localhost:5000/', {
+   
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
